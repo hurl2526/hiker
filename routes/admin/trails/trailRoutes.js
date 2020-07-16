61fc86496b1978a7a9362c45c3e9ddcf3eb94a6f
Patrick <patrick.hurley@codeimmersives.com>
2020-07-16 10:29:57 -0400

    parsing api data

--- routes/admin/trails/trailRoutes.js
+++ routes/admin/trails/trailRoutes.js
@@ -17,11 +17,11 @@ router.get('/:zipCode',async (req,res,next)=>{
   let zip = req.params.zipCode
   console.log(zip)
   // const url = `https://prescriptiontrails.org/api/filter/?zip=87102&by=${zip}&offset=0&count=6`
-  // const url =`https://prescriptiontrails.org/api/filter/?by=city&city=Albuquerque&offset=0&count=6`
-  const url = `https://prescriptiontrails.org/api/filter/?zip=${zip}&by=zip&offset=0&count=6`
+  const url =`https://prescriptiontrails.org/api/filter/?by=city&city=Albuquerque&offset=0&count=6`
+  // const url = `https://prescriptiontrails.org/api/filter/?zip=${zip}&by=zip&offset=0&count=6`
   const result = await axios.get(url);
   let data = result.data
-  console.log(data.trails)
+  console.log(data)
   return res.render("main/trails", {data})
   }catch(err){
     next(err)
